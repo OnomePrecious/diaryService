8db64ec9e59cbab5e70ca9932600e2306c24cfb1
Precious <preciousonome002@gmail.com>
2024-03-20 08:34:41 +0100

    Diary service

--- src/data/repositories/DiaryRepositoryImp.java
+++ src/data/repositories/DiaryRepositoryImp.java
@@ -43,7 +43,6 @@ public class DiaryRepositoryImp implements DiaryRepository{
                 diaries.remove(username);
                 return diary;
             }
-
         }
         return null;
     }
