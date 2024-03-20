b56b465bc5e9ee407ad56464045f20b17a91b15f
Precious <preciousonome002@gmail.com>
2024-03-21 00:00:43 +0100

    Diary service

--- test/data/repositories/EntryRepositoryTest.java
+++ test/data/repositories/EntryRepositoryTest.java
@@ -4,6 +4,9 @@ import data.model.Diary;
 import data.model.Entry;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EntryRepositoryTest {
@@ -38,13 +41,24 @@ class EntryRepositoryTest {
     }
 
     @Test
-   public void test_deleteByEntry() {
+    public void test_deleteByEntry() {
         EntryRepository repository = new EntryRepositoryImp();
         Entry entry = new Entry(1, "Had i known", "Anonymous");
         repository.save(entry);
         repository.delete(entry);
         assertNull(repository.findById(1));
+    }
 
-
+    @Test
+    public void testThatICanFindAllEntries() {
+        EntryRepository repository = new EntryRepositoryImp();
+        Entry entry = new Entry(1, "Had i known", "Anonymous");
+        Entry entry1 = new Entry(2, "Precious", "The story of my life");
+        repository.save(entry);
+        repository.save(entry1);
+        List<Entry> entries = new ArrayList<>();
+        entries.add(entry);
+        entries.add(entry1);
+        assertEquals(entries, repository.findAll());
     }
 }
